3879dfe3cdee7e65afe1423f606d6180f557ead4
Amin <fpAmin@gmail.com>
2026-03-05 03:21:59 +0100

    Commit, por que me voy de erasmun, tengo que subir esta  versión casi terminada.

--- añadi/GenericResponse.java
+++
@@ -1,6 +0,0 @@
-package com.example.controlpresencia;
-
-// Respuesta genérica del servidor para operaciones que solo devuelven un mensaje de confirmación
-public class GenericResponse {
-    public String msg;
-}
